061131459e5eee8cd7b7438a7f7caead8866e9f5
Johannes Schaller <jschalle@hotmail.de>
2021-08-03 18:14:10 +0200

    Add interface definitions
    Added interface definitions nsc, kreatur and waffen.

---
+++ src/assets/types/nsc.ts
@@ -0,0 +1,31 @@
+interface nsc {
+    name: string;
+
+    // Eigenschaften
+    mu: number;
+    kl: number;
+    in: number;
+    ch: number;
+    ff: number;
+    ge: number;
+    ko: number;
+    kk: number;
+
+    // Wichtige Werte
+    lep: number;
+    asp: number;
+    kap: number;
+    ini: number;
+    rs: number;
+    be: number;
+    sk: number;
+    zk: number;
+    gs: number;
+    aw: number;
+
+    nahkampfwaffen: nahkampfwaffe[];
+    fernkampfwaffen: fernkampfwaffe[];
+
+    aktionen: number;
+    groesse: string;
+}
---
+++ src/assets/types/waffen.ts
@@ -0,0 +1,15 @@
+interface nahkampfwaffe {
+    name: string;
+    at: string;
+    pa?: string;
+    tp: number[];
+    rw: string;
+}
+
+interface fernkampfwaffe {
+    name: string;
+    fk: number;
+    lz: number;
+    tp: number[];
+    rw: number[];
+}
