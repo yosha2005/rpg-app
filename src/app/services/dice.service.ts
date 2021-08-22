c3ed11efdc10b2553744849e8ef5ffdf20d13767
Johannes Schaller <jschalle@hotmail.de>
2021-08-22 21:14:54 +0200

    Add roll functionality to dice service

--- src/app/services/dice.service.ts
+++ src/app/services/dice.service.ts
@@ -6,4 +6,14 @@ import { Injectable } from '@angular/core';
 export class DiceService {
 
   constructor() { }
+
+  roll(numberOfDice: number, sides: number) {
+    const results = [];
+
+    for (let i = 0; i < numberOfDice; i++) {
+      results.push(Math.floor(Math.random() * sides));
+    }
+
+    return results;
+  }
 }
