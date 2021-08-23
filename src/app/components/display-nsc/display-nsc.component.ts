de6ffe4eb68d72e15cde686462d876188ad7e204
Johannes Schaller <jschalle@hotmail.de>
2021-08-23 10:56:44 +0200

    Add roll attack and roll parry to display nsc component

--- src/app/components/display-nsc/display-nsc.component.ts
+++ src/app/components/display-nsc/display-nsc.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+
+import { DiceService } from 'src/app/services/dice.service';
+
 import { nsc } from 'src/assets/types/nsc';
 
 @Component({
@@ -8,4 +11,31 @@ import { nsc } from 'src/assets/types/nsc';
 })
 export class DisplayNscComponent {
   @Input() nsc = <nsc>{};
+
+  constructor(private dice: DiceService) { }
+
+  rollAttack(at: number | any, tp: number[]) {
+    const result = this.dice.roll(1, 20);
+
+    if (at >= result[0]) {
+      console.log('Attacke erfolgreich');
+
+      let trefferpunkte = 0;
+      this.dice.roll(tp[0], tp[1]).forEach(el => trefferpunkte += el);
+      trefferpunkte += tp[2];
+      console.log('TP: ' + trefferpunkte);
+    } else {
+      console.log('Attacke fehlgeschlagen');
+    }
+  }
+
+  rollParry(pa: number | any) {
+    const result = this.dice.roll(1, 20);
+
+    if (pa >= result[0]) {
+      console.log('Parade erfolgreich');
+    } else {
+      console.log('Parade fehlgeschlagen');
+    }
+  }
 }
