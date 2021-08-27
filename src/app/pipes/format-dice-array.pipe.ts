e9938977081127ff6faea89b6d5467a2f454b070
Johannes Schaller <jschalle@hotmail.de>
2021-08-27 13:15:35 +0200

    Implement formatDiceArray pipe functionality
    The pipe converts a dice array - which is used to define a dice
    roll - into a easily readable string.

--- src/app/pipes/format-dice-array.pipe.ts
+++ src/app/pipes/format-dice-array.pipe.ts
@@ -5,8 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormatDiceArrayPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: number[]): string {
+    let diceString: string = '';
+
+    diceString += String(value[0]) + 'W' + String(value[1]);
+
+    if (value[2] != undefined) {
+      const mod: number = Math.abs(value[2]);
+      if (value[2] > 0) {
+        diceString += '+' + mod;
+      } else if (value[2] < 0) {
+        diceString += '-' + mod;
+      }
+    }
+
+    return diceString;
   }
 
 }
